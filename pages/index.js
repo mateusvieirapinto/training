4f6aabfed4f2e3e305fbfa96c12093a6b6611e8f
mateusvieirapinto <125665271+mateusvieirapinto@users.noreply.github.com>
2026-03-03 20:43:34 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home() {
+    return <h1>Raiane, eu amo você, se você me ama da uma risadinha</h1>
+    return <p>Ei amigo, você sabe onde fica a rua cthurusbango arusmango?</p>
+}
+
+export default Home;
