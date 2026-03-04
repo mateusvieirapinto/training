b1f107a78b47620b9a9b59e30cef013af7d671c3
mateusvieirapinto <125665271+mateusvieirapinto@users.noreply.github.com>
2026-03-04 11:56:06 +0000

    alterado h1

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
-    return <h1>Raiane, eu amo você, se você me ama da uma risadinha</h1>
-    return <p>Ei amigo, você sabe onde fica a rua cthurusbango arusmango?</p>
+    return <h1>Ei amigo, você sabe onde fica a rua cthurusbango arusmango?</h1>
 }
 
 export default Home;
