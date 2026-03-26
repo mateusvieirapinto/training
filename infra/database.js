2d1334c16a5ae6d27c1ab29fc514eef8ad1862c4
mateusvieirapinto <125665271+mateusvieirapinto@users.noreply.github.com>
2026-03-26 00:10:12 +0000

    finish `/api/v1/status` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,19 +1,19 @@
-import { Client } from "pg";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  max: 10, // limite de conexões
+});
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
+  const result = await pool.query(queryObject);
   return result;
 }
 
 export default {
-  query: query,
+  query,
 };
